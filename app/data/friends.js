3dcbe6a5c5de6c0d700748bd8bb7afaca6203fc2
peiwangboston <wangpeius@yahoo.com>
2017-09-17 20:03:08 -0400

    homework -express

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+  {
+    name: 'mr. wang',
+    profilePic: 'https://www.pexels.com/photo/scenic-view-of-beach-248797',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
