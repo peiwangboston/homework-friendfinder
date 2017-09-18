aad7bde5a0160d0ce2449849661f296e24c8c4f9
peiwangboston <wangpeius@yahoo.com>
2017-09-17 20:57:44 -0400

    Update htmlroutes.js

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -9,8 +9,7 @@ module.exports = function(app){
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 
-  //a USE route to home page
-  app.use(function (req, res) {
+   app.get(function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
 };
